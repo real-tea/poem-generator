97e054ff7da4c1552a34de3f6c480958e4b8a230
real-tea <akash.0711a@gmail.com>
2023-06-27 10:45:20 +0530

    loader json

--- App.js
+++ App.js
@@ -33,20 +33,88 @@ export default function App() {
     setPoem("");
   }
 
-  
+
 
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-black">
+    <Text className="text-2xl font-bold italic text-white">
+      Poem Generator
+
+    </Text>
+
+    <Text className="text-sm font-bold italic text-center text-white">
+      Express your feelings ❣️
+    </Text>
+
+    <View className="w-10/12">
+    {!showPoem ? (
+      <Formik
+        validationSchema={requestValidationSchema}
+        initialValues={{name : "" , toName : "" , feelings : ""}}
+        onSubmit={handleSubmit}
+        >
+        {({ handleChange , handleBlur , handleSubmit  , values , errors }) => (
+          <View>
+            <Input
+              value = {values.name}
+              onBlur = {handleBlur("toName")}
+              onChangeText={handleChange("toName")}
+              placeholder="Who are you sending this to?"
+            />
+
+            {errors.toName && (
+              <Text className="text-red-500 mt-1">{errors.toName}</Text>
+            )}
+            <Input
+              style={{
+                height : 100 , 
+                textAlignVertical : "top",
+              }}
+              placeholder = "What are your feelings?"
+              onChangeText = {handleChange("feelings")}
+              onBlur={handleBlur("feelings")}
+              value={values.feelings}
+              multiline={true}
+            />
+            {errors.feelings && (
+              <Text className="text-red-500 mt-1">{errors.feelings}</Text>
+            )}
+            <CustomButton
+            style={{
+              flexDirection : "row",
+            }}
+            onPress={handleSubmit}
+            title="Generate">
+
+            </CustomButton>
+            {loading && (
+              <View
+              style={[
+                styleSheet.absoluteFillObject ,
+                {
+                  justifyContent : "center",
+                  alignItems : "center",
+                  backgroundColor : "rgba(0,0,0,0.5)"
+                },
+              ]}>
+
+                <LottieView
+                  sorce={require("./assets/heart-loader.json")}
+                />
+
+              </View>
+            )}
+
+            
+          </View>
+        )}
+
+      </Formik>
+    )
+
+    }
+    </View>
       
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
